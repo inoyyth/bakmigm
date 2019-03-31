92c2861d52f9e0fe21d38ed8f0a13d78c4d38401
supriyadin <supri170845@gmail.com>
2019-03-31 11:10:10 +0700

    update download button

--- application/views/V_menu.php
+++ application/views/V_menu.php
@@ -153,9 +153,9 @@
 					<td><?php echo $aging;?></td>
 					<td>
 						<a href="<?php echo base_url('document-details-'.$key->DOC_ID); ?>" class="fa fa-eye" style="font-size: 2rem;text-decoration: none;color: black;" target="_blank"></a>
-						<?php if (strrpos($key->JBLL_DOWNLOAD, $this->session->userdata("session_bgm_edocument_job_level_id")) !== FALSE): ?>
+						<?php //if (strrpos($key->JBLL_DOWNLOAD, $this->session->userdata("session_bgm_edocument_job_level_id")) !== FALSE): ?>
 						<a style="font-size: 2rem;text-decoration: none;color: black;" class="fa fa-download" href="<?=base_url('download-'.$key->DOC_ID.".zip");?>" id="btn-unduh" class="btn btn-sm btn-warning"></a>
-						<?php endif; ?>
+						<?php //endif; ?>
 					</td>
 				</tr>
 			</tbody>
